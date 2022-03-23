85c06c47e185114204cdf6be8c90b8b3240f40ef
= <danishfaisal.wwe@gmail.com>
2022-03-23 22:48:38 +0530

    ContactList generated from static data

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,53 @@ import ContactList from './ContactList';
 import Header from './Header';
 
 function App() {
+  const contacts = [{
+    "id": 1,
+    "name": "Logan Tothacot",
+    "email": "ltothacot0@yahoo.com"
+  }, {
+    "id": 2,
+    "name": "Hale Bellefonte",
+    "email": "hbellefonte1@examiner.com"
+  }, {
+    "id": 3,
+    "name": "Saunderson Ebbers",
+    "email": "sebbers2@nhs.uk"
+  }, {
+    "id": 4,
+    "name": "Maridel McGeraghty",
+    "email": "mmcgeraghty3@foxnews.com"
+  }, {
+    "id": 5,
+    "name": "Godfry Tacey",
+    "email": "gtacey4@pcworld.com"
+  }, {
+    "id": 6,
+    "name": "Andrus D'Agostino",
+    "email": "adagostino5@comsenz.com"
+  }, {
+    "id": 7,
+    "name": "Fredek Eyam",
+    "email": "feyam6@sbwire.com"
+  }, {
+    "id": 8,
+    "name": "Blondy Caldaro",
+    "email": "bcaldaro7@ustream.tv"
+  }, {
+    "id": 9,
+    "name": "Ula Fuchs",
+    "email": "ufuchs8@addtoany.com"
+  }, {
+    "id": 10,
+    "name": "Sauncho Sebright",
+    "email": "ssebright9@diigo.com"
+  }];
+
   return (
     <div className="ui container">
       <Header />
       <AddContact />
-      <ContactList />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-const ContactList = () => {
+const ContactList = (props) => {
+    const renderContactList = props.contacts.map((contact) => (
+        <div key={contact.id} className="item">
+            <div className="content">
+                <div className="header">{contact.name}</div>
+                <div>{contact.email}</div>
+            </div>
+            <i className="trash alternate outline icon"></i>
+        </div>
+    ));
+
     return (
         <div className="ui celled list">
-            Contact List
+            {renderContactList}
         </div>
     );
 }
