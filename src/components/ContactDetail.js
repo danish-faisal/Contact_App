24b26e95d03c9f171ba6691563f4e2f12eaa8619
= <danishfaisal.wwe@gmail.com>
2022-04-01 22:39:32 +0530

    Link from ContactDetail to ContactList

--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import user from "../images/user.jpg";
 
 const ContactDetail = (props) => {
     const location = useLocation();
-    const data = location.state;
-    const {name, email} = data.contact;
+    const contactData = location.state;
+    const { name, email } = contactData;
     return (
         <div className="main">
-        <div className="ui card centered">
-            <div className="image">
-            <img src={user} alt="user" />
+            <div className="ui card centered">
+                <div className="image">
+                    <img src={user} alt="user" />
+                </div>
+                <div className="content">
+                    <div className="header">{name}</div>
+                    <div className="description">{email}</div>
+                </div>
             </div>
-            <div className="content">
-            <div className="header">{name}</div>
-            <div className="description">{email}</div>
+            <div className="center-div">
+                <Link to="/">
+                    <button className="ui button blue center">
+                        Back to Contact List
+                    </button>
+                </Link>
             </div>
-        </div>
         </div>
     );
 }
