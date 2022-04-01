6a2f51a7168369d67948bb5b97d1f7ed9ca6bde7
= <danishfaisal.wwe@gmail.com>
2022-04-01 22:26:06 +0530

    Pass contact details to ContactDetail page

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -9,7 +9,7 @@ const ContactCard = (props) => {
         <div className="item">
             <img className="ui avatar image" src={user} alt="user" />
             <div className="content">
-                <Link to={`/contact/${id}`}>
+                <Link to={`/contact/${id}`} state={props.contact}>
                     <div className="header">{name}</div>
                     <div>{email}</div>
                 </Link>
--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import user from "../images/user.jpg";
 
-const ContactDetail = () => {
-  return (
-    <div className="main">
-      <div className="ui card centered">
-        <div className="image">
-          <img src={user} alt="user" />
+const ContactDetail = (props) => {
+    const location = useLocation();
+    const data = location.state;
+    const {name, email} = data.contact;
+    return (
+        <div className="main">
+        <div className="ui card centered">
+            <div className="image">
+            <img src={user} alt="user" />
+            </div>
+            <div className="content">
+            <div className="header">{name}</div>
+            <div className="description">{email}</div>
+            </div>
         </div>
-        <div className="content">
-          <div className="header">Danish</div>
-          <div className="description">danishfaisal.wwe@gmail.com</div>
         </div>
-      </div>
-    </div>
-  )
+    );
 }
 
 export default ContactDetail;
