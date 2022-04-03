78c0546817a009c04ad65840c28706a97f00bf39
= <danishfaisal.wwe@gmail.com>
2022-04-03 11:56:21 +0530

    Delete Contact using API

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,8 @@ function App() {
     setContacts([...contacts, response.data]);
   }
 
-  const removeContactHandler = (id) => {
+  const removeContactHandler = async (id) => {
+    await api.delete(`/contacts/${id}`);
     const newContactList = contacts.filter((contact) => contact.id !== id);
     setContacts(newContactList);
   }
