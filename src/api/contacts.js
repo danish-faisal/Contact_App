6d39cb4f232c61db366673a694f0ca86e13f5238
Danish Faisal <43044452+danish-faisal@users.noreply.github.com>
2022-09-10 20:16:39 +0530

    Server moved to Render

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -3,5 +3,6 @@ import axios from "axios";
 export default axios.create({
     // baseURL: "http://localhost:3006/",
     // baseURL: "https://my-json-server.typicode.com/danish-faisal/ContactApp_Server/",
-    baseURL: "https://contactapp-server-backend.herokuapp.com/"
+   // baseURL: "https://contactapp-server-backend.herokuapp.com/",
+    baseURL: "https://contactapp-server-backend.onrender.com/"
 });
